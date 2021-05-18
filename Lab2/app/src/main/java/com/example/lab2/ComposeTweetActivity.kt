36547deebf40a4f787657a1b6c221256a68c39bc
Nicoleta <nicolettalupu99@gmail.com>
2021-05-18 21:15:07 +0300

    added lab2

---
+++ Lab2/app/src/main/java/com/example/lab2/ComposeTweetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ComposeTweetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_compose_tweet)
+    }
+}
---
+++ Lab2/app/src/main/java/com/example/lab2/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.example.lab2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.example.lab2.helpers.ThemeChecker
+import com.example.lab2.helpers.getSharedPref
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        ThemeChecker().darkModeChecker(this)
+
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            Log.e("pref", getSharedPref("access_token").toString())
+            if (getSharedPref("access_token") != null) {
+                startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+            } else {
+                startActivity(Intent(this@SplashActivity, IntroActivity::class.java))
+            }
+            finish()
+        }, 1000)
+    }
+}
